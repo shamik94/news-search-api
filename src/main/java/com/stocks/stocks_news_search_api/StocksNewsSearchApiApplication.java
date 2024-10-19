9f4a068e400da5a50ff2479cabed9f3b747c6a5e
“ShamikMukhopadhyay” <shamik.mukhopadhyay@deliveryhero.com>
2024-10-19 15:49:10 +0200

    initial commit

---
+++ src/main/java/com/stocks/stocks_news_search_api/StocksNewsSearchApiApplication.java
@@ -0,0 +1,13 @@
+package com.stocks.stocks_news_search_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StocksNewsSearchApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StocksNewsSearchApiApplication.class, args);
+	}
+
+}
